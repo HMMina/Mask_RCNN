ee443349d277ebd6c56cc5f36db0f58bf70d9056
Hoàng Anh Minh <150469633+HMMina@users.noreply.github.com>
2025-05-28 04:55:41 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,16 @@
-"""
-The build/compilations setup
-
->> pip install -r requirements.txt
->> python setup.py install
-"""
-import pip
+from setuptools import setup, find_packages
 import logging
-import pkg_resources
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
+# Load requirements from file
+def load_requirements(filename):
+    try:
+        with open(filename) as f:
+            return f.read().splitlines()
+    except FileNotFoundError:
+        logging.warning("requirements.txt not found. Proceeding without it.")
+        return []
 
-def _parse_requirements(file_path):
-    pip_ver = pkg_resources.get_distribution('pip').version
-    pip_version = list(map(int, pip_ver.split('.')[:2]))
-    if pip_version >= [6, 0]:
-        raw = pip.req.parse_requirements(file_path,
-                                         session=pip.download.PipSession())
-    else:
-        raw = pip.req.parse_requirements(file_path)
-    return [str(i.req) for i in raw]
-
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-try:
-    install_reqs = _parse_requirements("requirements.txt")
-except Exception:
-    logging.warning('Fail load requirements file, so using default ones.')
-    install_reqs = []
+install_reqs = load_requirements("requirements.txt")
 
 setup(
     name='mask-rcnn',
@@ -39,10 +20,10 @@ setup(
     author_email='waleed.abdulla@gmail.com',
     license='MIT',
     description='Mask R-CNN for object detection and instance segmentation',
-    packages=["mrcnn"],
+    packages=find_packages(),
     install_requires=install_reqs,
     include_package_data=True,
-    python_requires='>=3.4',
+    python_requires='>=3.6',
     long_description="""This is an implementation of Mask R-CNN on Python 3, Keras, and TensorFlow. 
 The model generates bounding boxes and segmentation masks for each instance of an object in the image. 
 It's based on Feature Pyramid Network (FPN) and a ResNet101 backbone.""",
@@ -60,9 +41,11 @@ It's based on Feature Pyramid Network (FPN) and a ResNet101 backbone.""",
         "Topic :: Scientific/Engineering :: Image Recognition",
         "Topic :: Scientific/Engineering :: Visualization",
         "Topic :: Scientific/Engineering :: Image Segmentation",
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     keywords="image instance segmentation object detection mask rcnn r-cnn tensorflow keras",
 )
